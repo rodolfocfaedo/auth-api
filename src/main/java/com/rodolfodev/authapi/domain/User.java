69bf77fe7937f4bda2e8f29f4df8f04d364bc07f
RodolfoDev <rodolfo.cunhasque.f@gmail.com>
2026-06-24 17:24:58 -0300

    feat: add initial user authentication structure

---
+++ src/main/java/com/rodolfodev/authapi/domain/User.java
@@ -0,0 +1,45 @@
+package com.rodolfodev.authapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @Override
+    public @NonNull Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @NonNull String getUsername() {
+        return email;
+    }
+
+}
